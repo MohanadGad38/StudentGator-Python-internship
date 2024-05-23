5dad867e9fcc2106465a21e233eed09167a2eb5e
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-05-23 07:53:04 +0300

    c

--- 1/run.py
+++ 1/run.py
@@ -1,7 +1,11 @@
 # Classes and Instances for employees
 # describe the Difference  a class and instance of class
 # constructor and normal function
+# class variable and instance variable
+# classmethods and staticmethods
 class employee:
+    user_type: int = 1
+
     def __init__(self, name, age, email) -> None:
         self.name: str = name
         self.age: str = age
